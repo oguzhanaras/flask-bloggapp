de8f25c061daddfa852390c2f5abd7add0767e54
oguzhanaras <oguzhanarasx@gmail.com>
2024-10-15 23:20:51 +0300

    route, jinja, wtform, templates
    flask project was created. bootstrap cdn was integrated into layout.html and modular html structure was established. navbar was added. router was made. modules required for mysql were installed and connection was provided. database was created and users table was added. form was created with wtforms for registration form.

---
+++ blog.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+
+app = Flask(__name__)
+
+# Config MySQL
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'blog_db'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+
+# kullanıcı kayıt formu
+class RegisterForm(Form):
+    name = StringField("İsim Soyisim", validators=[validators.Length(min=4, max=25)])
+    username = StringField("Kullanıcı Adı", validators=[validators.Length(min=5, max=25)])
+    email = StringField("Email Adresi", validators=[validators.Email(message="Lütfen geçerli bir email adresi giriniz")])
+    password = PasswordField("Parola:", validators=[
+        validators.DataRequired(message="Lütfen bir parola belirleyin"),
+        validators.Length(min=6, max=30),
+        validators.EqualTo('confirm', message="Parolalar eşleşmiyor")
+    ])
+    confirm = PasswordField("Parolayı Doğrula")
+
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/article/<string:id>')
+def detail(id):
+    return f"article id: {id}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
